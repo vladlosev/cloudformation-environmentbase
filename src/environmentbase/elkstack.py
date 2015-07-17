afd0875c02fdbf6ea9785e8e6f2af682fc3ef867
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-17 13:36:54 -0700

    Adds EC2 resource to template.

--- src/environmentbase/elkstack.py
+++ src/environmentbase/elkstack.py
@@ -1,11 +1,20 @@
 from networkbase import NetworkBase
+from troposphere import ec2
 
 class ElkStack(NetworkBase):
     '''
     ELK stack template generation
     '''
 
+    def create_action(self):
+        self.initialize_template()
 
+        # Add some stuff
+        res = ec2.Instance("ec2instance", InstanceType="m3.medium", ImageId="ami-951945d0")
+        self.template.add_resource(res)
+
+        # This triggers serialization of the template and any child stacks
+        self.write_template_to_file()
 
 if __name__ == '__main__':
     ElkStack()
