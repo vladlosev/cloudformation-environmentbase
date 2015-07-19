ce5d7c8297871daa530edb19d88cc0fdc51e3214
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-18 20:07:11 -0700

    Updates kibana to 4.1.1.

--- src/environmentbase/elkstack.py
+++ src/environmentbase/elkstack.py
@@ -11,7 +11,7 @@ class ElkStack(NetworkBase):
 
         self.create_logstash()
         self.create_kibana()
-        self.create_elastisearch()
+        self.create_elasticsearch()
 
         # This triggers serialization of the template and any child stacks
         self.write_template_to_file()
@@ -46,9 +46,9 @@ service logstash restart
 
         mkdir -p /opt/kibana
         cd /opt/kibana
-        wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
+        wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
         tar xvf kibana-*.tar.gz
-        mv kibana-4.0.1-linux-x64/* ./
+        mv kibana-4.1.1-linux-x64/* ./
 
         cd /etc/init.d && wget https://gist.githubusercontent.com/orih/182da221010f56e4644e/raw/b42ab63fd40c8b4f18289aa91bb92d55bb1e0c5d/kibana.sh
         mv /etc/init.d/kibana.sh /etc/init.d/kibana
@@ -60,15 +60,15 @@ service logstash restart
             Tags=Tags(Name="kibana",), UserData=Base64(kibana_startup))
         self.template.add_resource(res)
 
-    def create_elastisearch(self):
+    def create_elasticsearch(self):
         elasticsearch_startup = '''#!/bin/bash
 
 rpm --import https://packages.elasticsearch.org/GPG-KEY-elasticsearch
 
 cat <<EOF >> /etc/yum.repos.d/elasticsearch.repo
-[elasticsearch-1.4]
-name=Elasticsearch repository for 1.4.x packages
-baseurl=http://packages.elasticsearch.org/elasticsearch/1.4/centos
+[elasticsearch-1.7]
+name=Elasticsearch repository for 1.7.x packages
+baseurl=http://packages.elasticsearch.org/elasticsearch/1.7/centos
 gpgcheck=1
 gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
 enabled=1
